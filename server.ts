65aff48bdc7a8f1124435d055d981d9feb27f9aa
singhthedev <vishal.singh@technogetic.com>
2024-02-20 15:31:14 +0530

    added cluster in the code

--- server.ts
+++ server.ts
@@ -1,29 +1,39 @@
-import express  from 'express';
+import express from 'express';
 import './src/config/db';
 import bodyParser from 'body-parser';
 import { configCorsOptions } from './src/config/corsConfig';
 import { environmentConfig } from './src/config/envConfig';
-
-
+import cluster from 'cluster';
+import os from 'os';
 import router from './src/router/allRoute';
 
+const numCPUs = os.cpus().length;
 
-const app = express();
+if (cluster.isPrimary) {
+    console.log(`Master ${process.pid} is running...👍️`);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(configCorsOptions());
-
-app.use('/api/v1', router)
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-
-app.listen(environmentConfig.SERVER_PORT || 3000, () => {
-    console.log(`Server is running...🚀`);
-    const error = false;
-    if (error) {
-        console.log(`Server error...🫣`,error);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-})
+
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(configCorsOptions());
+
+    app.use('/api/v1', router);
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+
+    app.listen(environmentConfig.SERVER_PORT || 3000, () => {
+        console.log(`Worker ${process.pid} started...🚀`);
+    });
+}
+
